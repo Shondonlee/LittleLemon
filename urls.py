578d3759bc85cea2547e5a2d3fc9db3e394ce0fa
Shondonlee <125543913+Shondonlee@users.noreply.github.com>
2023-02-16 15:53:02 +0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,25 +1,23 @@
-"""littlelemon URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('LittleLemonAPI.urls')),
-    path('restaurant/', include('restaurant.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('', views.index, name='home'),
+    
+    # api endpoints
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleItemView.as_view()),
+    path('booking/', include(router.urls)),
+    # path('booking/tables', views.BookingViewSet.as_view({
+    #         'get': 'list'
+        # })),
+        
+    # dubious instructions...
+    # tokens are already issued through the api paths.  kepp just in case for now.
+    path('api-token-auth/', obtain_auth_token),
 ]
